ef736acd91a2a0b13eaede4fec9dd0ee0be505d6
= <=>
2025-11-18 12:43:24 +0100

    feat: set up routing and add Demo, Dogs, and Dogdetail pages
    This update adds the app structure. The Dogs page now fetches data from db.json. A bottom nav bar has also been added.  Routing is now structured so the
    Demo page shows first, and the app pages are under "/app".

---
+++ src/pages/Demo.jsx
@@ -0,0 +1,24 @@
+import { useNavigate } from "react-router-dom"
+
+export default function Demo() {
+  const navigate = useNavigate()
+
+  function handleSkip() {
+    navigate("/app/dogs")
+  }
+
+  return (
+    <div className="demo-screen">
+      <img className="demo-img" src="./Animal.svg"/>
+      <h2>My Pets</h2>
+      <p>
+      Taking care of a pet is my favorite, 
+      it helps me to gaimr stress and fatigue.
+      </p>
+
+      <button className="skip-btn" onClick={handleSkip}>
+        Skip
+      </button>
+    </div>
+  )
+}
---
+++ src/pages/Dogs.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react"
+import { NavLink } from "react-router-dom"
+
+
+export default function Dogs() {
+  const [user, setUser] = useState(null)
+  const [dogs, setDogs] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const respone = await fetch("/db.json") 
+        const data = await respone.json()
+
+        setUser(data.user)
+        setDogs(data.dogs)
+      } catch (error) {
+        console.error("Error fetching dogs:", error)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+
+  return (
+    <div className="dogs-page">
+      {/* User info */}
+      {user && (
+        <div className="user-card">
+          <img src={user.image} alt="User" width="80" />
+          <p>Location: {user.location}</p>
+        </div>
+      )}
+
+      <h2>Available Dogs</h2>
+
+      <div className="dogs-list">
+        {dogs.map((dog) => (
+          <div key={dog.id} className="dog-card">
+            <img src={dog.image} alt={dog.breed} width="120" />
+
+            <h3>{dog.breed}</h3>
+            <p>{dog.location}</p>
+            <p>{dog.short_description}</p>
+          </div>
+        ))}
+      </div>
+      
+    <footer>
+        <nav className="bottom-nav">
+          <NavLink className="Link" to="/app/dogs" >
+            <img className="bottom-nav__con" src="/homepet.png" />
+          </NavLink>
+
+          <NavLink className="Link" to="/app">
+            <img className="bottom-nav__icon" src="/mas.png" />
+          </NavLink>
+
+          <NavLink className="Link" to="/app">
+            <img className="bottom-nav__icon" src="/fav.png" />
+          </NavLink>
+
+          <NavLink className="Link" to="/app">
+            <img className="bottom-nav__icon" src="/petprofil.png" />
+          </NavLink>
+        </nav>
+        </footer>
+    </div>
+  )
+}
+
